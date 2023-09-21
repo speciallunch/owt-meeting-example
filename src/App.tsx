4643e567e2ea74173f075e8810aca33cf38709cc
jihoon_sung <jihoon_sung@tmax.co.kr>
2023-09-21 13:32:09 +0900

    feat: Add getUserMedia for local stream
    - add getUserMedia for local stream
    - show video of local stream

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,36 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useRef, useState } from "react";
+import "./App.css";
 
 function App() {
+  const constraints = {
+    video: true,
+    audio: true,
+  };
+  const videoRef = useRef<HTMLVideoElement>(null);
+  const [myStream, setMyStream] = useState<MediaStream | undefined>(undefined);
+
+  function getUserStream() {
+    navigator.mediaDevices
+      .getUserMedia(constraints)
+      .then((stream) => {
+        console.log("Got MediaStream:", stream);
+        setMyStream(stream);
+      })
+      .catch((error) => {
+        // 권한이 거부되면 PermissionDeniedError
+        // 연결된 기기가 연결되어 있지 않으면 NotFoundError
+        console.error("Error accessing media devices.", error);
+      });
+  }
+
+  useEffect(() => {
+    if (!videoRef.current) return;
+    videoRef.current.srcObject = myStream ?? null;
+  }, [myStream]);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button onClick={getUserStream}>getUserStream</button>
+      <video autoPlay ref={videoRef} />
     </div>
   );
 }
